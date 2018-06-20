0a8bd94ffc50aad8807a09722cd56172cf56c961
trolleway <trolleway@yandex.ru>
2018-06-21 01:07:05 +0300

    Работает без ffprobe

--- __main__.py
+++ __main__.py
@@ -4,9 +4,43 @@ import subprocess
 from gooey import Gooey, GooeyParser
 import tempfile
 
+
+import subprocess
+import re
+from decimal import Decimal
+ 
+ 
 running = True
 
 
+def get_video_length(filepath):
+    cmd = 'ffmpeg -i "{filepath}" '.format(filepath=filepath)
+    out = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    stdout,stderr = out.communicate()
+    lines = stdout
+    metadata = {}
+    for l in lines.splitlines():
+        l = l.strip()
+        if l.startswith('Duration'):
+            metadata['duration'] = re.search('Duration: (.*?),', l).group(0).split(':',1)[1].strip(' ,')
+            t = metadata['duration'].split(':')
+            seconds = float( (t[0]*60*60)+(t[1]*60)+t[2] )
+            
+            metadata['bitrate'] = re.search("bitrate: (\d+ kb/s)", l).group(0).split(':')[1].strip()
+        if l.startswith('Stream #0:0'):
+            metadata['video'] = {}
+            metadata['video']['codec'], metadata['video']['profile'] = \
+                [e.strip(' ,()') for e in re.search('Video: (.*? \(.*?\)),? ', l).group(0).split(':')[1].split('(')]
+            metadata['video']['resolution'] = re.search('([1-9]\d+x\d+)', l).group(1)
+            metadata['video']['bitrate'] = re.search('(\d+ kb/s)', l).group(1)
+            metadata['video']['fps'] = re.search('(\d+ fps)', l).group(1)
+        if l.startswith('Stream #0:1'):
+            metadata['audio'] = {}
+            metadata['audio']['codec'] = re.search('Audio: (.*?) ', l).group(1)
+            metadata['audio']['frequency'] = re.search(', (.*? Hz),', l).group(1)
+            metadata['audio']['bitrate'] = re.search(', (\d+ kb/s)', l).group(1)
+    return seconds
+    
 @Gooey(optional_cols=2, program_name="concatenate videos")
 def main():
     settings_msg = "Concatenate videos in FFMPEG whthout re-encoding \n" \
@@ -51,6 +85,8 @@ def main():
             for video in videos_list:
                 extension = os.path.splitext(video)[1]
                 tf = tempfile.NamedTemporaryFile(suffix=extension,delete=False)
+                
+                '''
                 cmd = 'ffprobe -i "{video}" -show_entries format=duration -v quiet -of csv="p=0"'.format(video=video)
                 #duration = subprocess.check_output(['ffprobe', '-i', video,'-show_entries format=duration -v quiet -of csv="p=0"'])
                 #out = subprocess.Popen(['ffprobe', '-i', video,' -show_entries format=duration -v quiet -of csv="p=0"'], 
@@ -60,9 +96,16 @@ def main():
                 stdout,stderr = out.communicate()
                 duration = float(stdout)
                 #print duration
-                
-                
-                cmd = 'ffmpeg -ss {seconds} -i "{video}" -t {duration}  {output}'.format(video=video, seconds = seconds, output=tf.name+extension, duration=str(duration-float(seconds)*2))
+                '''
+                duration = get_video_length(video)
+               
+                if float(duration) > 5:
+                    cmd = 'ffmpeg -ss {seconds} -i "{video}" -t {duration} -qscale:v 2 {output}'.format(video=video, seconds = seconds, output=tf.name+extension, duration=str(duration-float(seconds)*2))
+                elif 3 <= float(duration) <= 5:
+                    cmd = 'ffmpeg -ss {seconds} -i "{video}" -t {duration} -qscale:v 2 {output}'.format(video=video, seconds = seconds, output=tf.name+extension, duration=str(duration-float(seconds)*2))
+                else:
+                    #3-second clips copying withoth clip
+                    cmd = 'ffmpeg -y -safe 0 -f concat -i {tf} -c copy  -vcodec copy  "{filename}"'.format(tf=video,filename=tf.name+extension)
                 os.system(cmd)
                 temp_videos.append(tf.name+extension)
                 print cmd
